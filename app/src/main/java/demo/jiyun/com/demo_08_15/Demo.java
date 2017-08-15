d562e00bb6458672e009c119e2a4b33a2766375b
王小帅 <1224964165@qq.com>
2017-08-15 09:40:54 +0800

    字分支 2017-8-15 09:40:48

---
+++ app/src/main/java/demo/jiyun/com/demo_08_15/Demo.java
@@ -0,0 +1,13 @@
+package demo.jiyun.com.demo_08_15;
+
+import android.util.Log;
+
+/**
+ * Created by 看的太透反而不快乐 on 2017/8/15.
+ */
+
+public class Demo {
+    public void get(){
+        Log.e("TAG", "新增Demo累" );
+    }
+}
